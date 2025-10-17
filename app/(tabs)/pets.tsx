e7bb901a3f65fa4d0920d96b225ef58b3ed15644
Thomas <thomashung076@gmail.com>
2025-10-17 16:53:46 +0100

    small modification

--- app/(tabs)/pets.tsx
+++ app/(tabs)/pets.tsx
@@ -1,17 +1,30 @@
 import { MenuButton } from '@/components/other/MenuButton';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import room_backgound from '@/assets/images/room_background.png';
+
 
 const Profile = () => {
   return (
-    <SafeAreaView className="flex-1 bg-white">
-      <View className="w-full flex-row items-center justify-between px-6 pt-4">
-        <MenuButton />
-        <View className="w-12" />
+    <SafeAreaView className="h-full bg-white">
+      <View className="w-full flex-row items-center px-6 pt-2">
+        <View className="flex-1">
+          <MenuButton />
+        </View>
+        <View className="flex-1 items-center">
+          <Text className="text-2xl font-rubik-medium text-gray-900">My Pets</Text>
+        </View>
+        <View className="flex-1 items-end" />
       </View>
-      <View className="flex-1 justify-center items-center">
-        <Text>Pets</Text>
+
+      <View className="-mt-2 items-start">
+        <Image
+          source={room_backgound}
+          className="w-full"
+          style={{ height: 400 }}
+          resizeMode="contain"
+        />
       </View>
     </SafeAreaView>
   );
