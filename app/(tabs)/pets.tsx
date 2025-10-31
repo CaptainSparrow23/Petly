e975c884eec1da43faf676e6ba6b6ea589b57da6
Thomas <thomashung076@gmail.com>
2025-10-31 02:33:56 +0000

    pets page

--- app/(tabs)/pets.tsx
+++ app/(tabs)/pets.tsx
@@ -1,16 +1,25 @@
 import React from "react";
-import { ImageBackground, View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
 import room_background from "@/assets/images/room_background.png";
+
 
 const PetsScreen = () => {
   return (
     <View className="flex-1 bg-gray-500">
       <ImageBackground
         source={room_background}
-        className="flex-1"
+        className="flex-1 mt-5"
         resizeMode="cover"
-        imageStyle={{ transform: [{ translateY: -70 }] }}
-      />
+        imageStyle={{ transform: [{ translateX: -4 }, { translateY: -250 }] }}
+      >
+        <View className="flex-1" />
+
+        <View className="px-8 pb-8 pt-6 rounded-t-3xl bg-white/90 h-[430px] shadow-2xl" >
+          <Text className="text-lg">
+            My Pets
+          </Text>
+        </View>
+      </ImageBackground>
     </View>
   );
 };
