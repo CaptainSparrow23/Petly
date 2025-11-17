76460f789a1105e4fcab6196def4f979fb9cc702
Thomas <thomashung076@gmail.com>
2025-11-17 19:39:21 +0000

    bug fixes

--- components/insights/TodayFocusCard.tsx
+++ components/insights/TodayFocusCard.tsx
@@ -37,12 +37,12 @@ export default function TodayFocusCard() {
   const durationLabel = useMemo(() => (totalSeconds ? formatDetailedDuration(totalSeconds) : ''), [totalSeconds]);
 
   return (
-    <View className="w-[60%] relative rounded-2xl border border-gray-200 bg-gray-50 p-3">
+    <View className="w-[65%] relative rounded-2xl border border-gray-200 bg-gray-50 p-3">
       <Text className="text-m text-gray-700">Today's Focus</Text>
       {totalSeconds > 0 ? (
-          <Text className="absolute bottom-2 right-3 text-3xl font-semibold font-rubik-bold text-blue-600">{durationLabel}</Text>
+          <Text className="absolute bottom-2 right-3 text-3xl font-semibold text-blue-600">{durationLabel}</Text>
       ) : (
-          <Text className="absolute bottom-2 right-3 text-3xl font-semibold font-rubik-bold text-blue-600">0 mins 0 secs</Text>
+          <Text className="absolute bottom-2 right-3 text-3xl font-semibold text-blue-600">0 mins 0 secs</Text>
       )}
     </View>
   );
